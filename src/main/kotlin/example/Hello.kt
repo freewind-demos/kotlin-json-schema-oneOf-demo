11fbe1308ee4101949e407ffce9622739fdf9c91
freewind <nowindlee@gmail.com>
2019-01-13 15:39:59 +0800

    demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,32 +1,27 @@
 package example
 
+import org.everit.json.schema.ValidationException
 import org.everit.json.schema.loader.SchemaLoader
 import org.json.JSONObject
 import org.json.JSONTokener
 import java.io.File
 
-val json = """
-{
-  "checked": false,
-  "dimensions": {
-    "width": 5,
-    "height": 10
-  },
-  "id": 1,
-  "name": "A green door",
-  "price": 12.5,
-  "tags": [
-    "home",
-    "green"
-  ],
-  "unknown-property": "should-not-allowed"
-}
-""".trimIndent()
+val jsons = listOf(
+        """{ "title": "something", "from": "somewhere", "to": "somewhere" }""",
+        """{ "title": "something", "address": "somewhere" }"""
+)
 
 fun main(args: Array<String>) {
     val jsonSchema = File("./schema.json").readText()
     val rawSchema = JSONObject(JSONTokener(jsonSchema))
     val schema = SchemaLoader.load(rawSchema)
-    schema.validate(JSONObject(json))
-    println("Validation passed")
+    jsons.forEach { json ->
+        try {
+            schema.validate(JSONObject(json))
+            println("$json: passed")
+        } catch (e: ValidationException) {
+            println("$json: failed: ${e.allMessages}")
+        }
+    }
+
 }
