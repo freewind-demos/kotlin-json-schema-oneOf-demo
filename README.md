8fca9078bb251d90133be82c6d0187e42095f267
freewind <nowindlee@gmail.com>
2019-01-13 14:30:18 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Kotlin Hello World Demo
-=======================
+Kotlin JSON Schema Validator Demo
+=================================
+
+Use online services like <https://www.jsonschema.net/> to generate json schema from json data.
+
+If you don't want unknown properties, you have to add `"additionalProperties": false` in your schema file.
 
 Run `Hello.kt` in your IDE.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,31 @@
 package example
 
-fun main(args: Array<String>) {
-    println(hello("Kotlin"))
-}
+import org.everit.json.schema.loader.SchemaLoader
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.File
 
-fun hello(name: String): String = "Hello, $name!"
+val json = """
+{
+  "checked": false,
+  "dimensions": {
+    "width": 5,
+    "height": 10
+  },
+  "id": 1,
+  "name": "A green door",
+  "price": 12.5,
+  "tags": [
+    "home",
+    "green"
+  ],
+  "unknown-property": "should-not-allowed"
+}
+""".trimIndent()
+
+fun main(args: Array<String>) {
+    val jsonSchema = File("./schema.json").readText()
+    val rawSchema = JSONObject(JSONTokener(jsonSchema))
+    val schema = SchemaLoader.load(rawSchema)
+    schema.validate(JSONObject(json))
+}
